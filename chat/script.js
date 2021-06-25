ea1a1b1d2c72d99679f9ecd133d473d9e694fe2e
Ian Lafferty <ianjlafferty@gmail.com>
2021-06-24 20:38:58 -0400

    last effort of trying to fix
    I don't know gave up trying to make things work together.

--- chat/script.js
+++ chat/script.js
@@ -1,9 +1,9 @@
-const socket = io('http://10.0.0.92:3000')
+const socket = io('http://localhost:3001')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
+const name = window.prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
